0c21777b989851aa47076970a484b625a354eaca
cl2832 <cl2832@cornell.edu>
2025-12-02 15:10:55 -0500

    change

--- src/Factory.jl
+++ src/Factory.jl
@@ -1,1 +1,43 @@
-throw(ErrorException("Oppps! No methods defined in src/Factory.jl. What should you do here?"))
+#throw(ErrorException("Oppps! No methods defined in src/Factory.jl. What should you do here?"))
+# -- PUBLIC METHODS BELOW HERE ---------------------------------------------------------------------------------------- #
+function build(modeltype::Type{MyClassicalHopfieldNetworkModel}, data::NamedTuple)::MyClassicalHopfieldNetworkModel
+
+    # initialize -
+    model = modeltype();
+    linearimagecollection = data.memories;
+    number_of_rows, number_of_cols = size(linearimagecollection);
+    W = zeros(Float32, number_of_rows, number_of_rows);
+    b = zeros(Float32, number_of_rows); # zero bias for classical Hopfield
+
+    # compute the W -
+    for j ∈ 1:number_of_cols # compute the outer product -
+        Y = zeros(Float32, number_of_rows, number_of_rows);
+        y = linearimagecollection[:,j];
+        for i ∈ 1:number_of_rows
+            for j ∈ 1:number_of_rows
+                if i != j
+                Y[i,j] = y[i]*y[j]
+                else
+                Y[i,j] = 0.0f0; # no self-coupling
+            end
+        end
+        W += Y; # update the W -
+    end
+    
+    WN = (1/number_of_cols)*W; # Hebbian scaling by number of memories stored
+    
+    # compute the energy dictionary -
+    energy = Dict{Int64, Float32}();
+    for i ∈ 1:number_of_cols
+        energy[i] = _energy(linearimagecollection[:,i], WN, b);
+    end
+
+    # add data to the model -
+    model.W = WN;
+    model.b = b;
+    model.energy = energy;
+
+    # return -
+    return model;
+end
+# --- PUBLIC METHODS ABOVE HERE -------------------------------------------------------------------------------------- #
--- src/Types.jl
+++ src/Types.jl
@@ -1,1 +1,13 @@
-throw(ErrorException("Oppps! No methods defined in src/Types.jl. What should you do here?"))
+#throw(ErrorException("Oppps! No methods defined in src/Types.jl. What should you do here?"))
+abstract type AbstractlHopfieldNetworkModel end
+
+mutable struct MyClassicalHopfieldNetworkModel <: AbstractlHopfieldNetworkModel
+
+    # data -
+    W::Array{<:Number, 2} # weight matrix
+    b::Array{<:Number, 1} # bias vector
+    energy::Dict{Int64, Float32} # energy of the states
+
+    # empty constructor -
+    MyClassicalHopfieldNetworkModel() = new();
+end
